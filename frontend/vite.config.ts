89f42bdd361d816271afef61e38fe3fa6b4f1e72
Thang Truong <thangtruong1808@gmail.com>
2026-02-27 01:24:15 +1100

    Fixed warning outdated JSX transform under browser console

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react({ jsxRuntime: 'automatic' })],
+  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: {
+      jsx: 'automatic',
+      jsxImportSource: 'react',
+    },
+    include: ['react-big-calendar'],
+  },
 })
